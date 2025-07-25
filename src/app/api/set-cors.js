c6fabb1e0661e08e86be3b047bb28cfb8a8b77f6
omoefe <raniem57@gmail.com>
2025-07-25 12:05:17 +0100

    switched firebase project

--- src/app/api/set-cors.js
+++
@@ -1,6 +0,0 @@
-import configureCors from "../../../setCors";
-
-export default async (req, res) => {
-  const success = await configureCors();
-  res.status(success ? 200 : 500).json({ success });
-};
